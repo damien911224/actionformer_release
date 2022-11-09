948d5a4c030ee9fd9d71b5df656eaf831925774b
damien911224 <damien911224@gmail.com>
2022-11-09 13:34:18 +0900

    Modified

--- libs/utils/train_utils.py
+++ libs/utils/train_utils.py
@@ -475,14 +475,14 @@ def valid_one_epoch(
             segments = list()
             for p, x in zip(output, video_list):
                 this_labels = p["labels"].float()
-                this_scores = p["scores"].float()
+                this_scores = p["scores"]
                 this_segments = p["segments"] / x["duration"]
                 if len(this_labels) < 100:
                     this_labels = F.pad(this_labels, (0, 100 - len(this_labels)))
                     this_scores = F.pad(this_scores, (0, 100 - len(this_scores)))
                     this_segments = F.pad(this_segments, (0, 0, 0, 100 - len(this_segments)))
                 elif len(this_labels) > 100:
-                    sorted_indices = torch.argsort(this_scores, dim=1, descending=True)[:100]
+                    sorted_indices = torch.argsort(this_scores, dim=0, descending=True)[:100]
                     this_labels = this_labels[sorted_indices]
                     this_scores = this_scores[sorted_indices]
                     this_segments = this_segments[sorted_indices]
