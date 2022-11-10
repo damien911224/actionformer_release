fc909186e5523f7a89fc9304aba6572d29f41d59
damien911224 <damien911224@gmail.com>
2022-11-10 18:40:01 +0900

    Modified

--- libs/utils/train_utils.py
+++ libs/utils/train_utils.py
@@ -303,11 +303,11 @@ def train_one_epoch(
             this_scores = p["scores"].float()
             this_segments = p["segments"] / x["duration"]
             if len(this_labels) < 4536:
-                this_labels = F.pad(this_labels, (0, 378 - len(this_labels)))
-                this_scores = F.pad(this_scores, (0, 378 - len(this_scores)))
-                this_segments = F.pad(this_segments, (0, 0, 0, 378 - len(this_segments)))
+                this_labels = F.pad(this_labels, (0, 4536 - len(this_labels)))
+                this_scores = F.pad(this_scores, (0, 4536 - len(this_scores)))
+                this_segments = F.pad(this_segments, (0, 0, 0, 4536 - len(this_segments)))
             elif len(this_labels) > 4536:
-                sorted_indices = torch.argsort(this_scores, dim=0, descending=True)[:378]
+                sorted_indices = torch.argsort(this_scores, dim=0, descending=True)[:4536]
                 this_labels = this_labels[sorted_indices]
                 this_scores = this_scores[sorted_indices]
                 this_segments = this_segments[sorted_indices]
@@ -503,11 +503,11 @@ def valid_one_epoch(
                 this_scores = p["scores"]
                 this_segments = p["segments"] / x["duration"]
                 if len(this_labels) < 4536:
-                    this_labels = F.pad(this_labels, (0, 378 - len(this_labels)))
-                    this_scores = F.pad(this_scores, (0, 378 - len(this_scores)))
-                    this_segments = F.pad(this_segments, (0, 0, 0, 378 - len(this_segments)))
+                    this_labels = F.pad(this_labels, (0, 4536 - len(this_labels)))
+                    this_scores = F.pad(this_scores, (0, 4536 - len(this_scores)))
+                    this_segments = F.pad(this_segments, (0, 0, 0, 4536 - len(this_segments)))
                 elif len(this_labels) > 4536:
-                    sorted_indices = torch.argsort(this_scores, dim=0, descending=True)[:378]
+                    sorted_indices = torch.argsort(this_scores, dim=0, descending=True)[:4536]
                     this_labels = this_labels[sorted_indices]
                     this_scores = this_scores[sorted_indices]
                     this_segments = this_segments[sorted_indices]
