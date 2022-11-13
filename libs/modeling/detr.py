6e7cdae34514d5b3b8f044d2f0018f385fdb45be
damien911224 <damien911224@gmail.com>
2022-11-13 22:41:43 +0900

    Modified

--- libs/modeling/detr.py
+++ libs/modeling/detr.py
@@ -181,7 +181,6 @@ class DINO(nn.Module):
         prop_label_embeds = self.label_enc(prop_labels.long())
         prop_score_embeds = self.score_enc(prop_scores)
         input_query_label = self.tgt_embed.weight.unsqueeze(0).repeat(features[0].size(0), 1, 1)
-        # input_query_label = input_query_label + prop_label_embeds + prop_score_embeds
         input_query_label = input_query_label + prop_label_embeds + prop_score_embeds
 
         # input_query_bbox = self.refpoint_embed.weight.unsqueeze(0).repeat(features.size(0), 1, 1)
