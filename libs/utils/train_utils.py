080dad6daa00ab974789b7f42899cb498ae68d47
damien911224 <damien911224@gmail.com>
2022-11-16 17:59:07 +0900

    Modified

--- libs/utils/train_utils.py
+++ libs/utils/train_utils.py
@@ -745,7 +745,7 @@ def valid_one_epoch_phase_2(
                 segments = torch.stack(segments, dim=0)
                 this_proposals = torch.cat((labels.unsqueeze(-1), segments, scores.unsqueeze(-1)), dim=-1).cuda()
                 proposals.append(this_proposals)
-            proposals = torch.cat(proposals, dim=1)
+            cat_proposals = torch.cat(proposals, dim=1)
 
             # features = [torch.stack([x["resize_feats"] for x in video_list], dim=0).cuda()]
             # features = [feat for feat in features]
@@ -755,7 +755,7 @@ def valid_one_epoch_phase_2(
             #                         for x in video_list], dim=0).cuda()
             # features = [features]
             features = [feat.detach() for feat in backbone_features]
-            detr_predictions = detr(features, proposals)
+            detr_predictions = detr(features, cat_proposals)
 
             boxes = detr_predictions["pred_boxes"].detach().cpu()
             boxes = (boxes[..., :2] +
@@ -766,6 +766,17 @@ def valid_one_epoch_phase_2(
             logits = detr_predictions["pred_logits"].detach().cpu().sigmoid()
             detr_scores, labels = torch.max(logits, dim=-1)
             scores = detr_scores
+
+            mean_proposals = torch.mean(torch.stack(proposals, dim=0), dim=0)
+            dense_boxes = mean_proposals[..., 1:3]
+            durations = [x["duration"] for x in video_list]
+            dense_boxes = dense_boxes * torch.Tensor(durations)
+            dense_scores = mean_proposals[..., -1]
+            dense_labels = mean_proposals[..., 0].long()
+
+            boxes = torch.cat((boxes, dense_boxes), dim=1)
+            scores = torch.cat((scores, dense_scores), dim=1)
+            labels = torch.cat((labels, dense_labels), dim=1)
 
             nmsed_boxes = list()
             nmsed_labels = list()
