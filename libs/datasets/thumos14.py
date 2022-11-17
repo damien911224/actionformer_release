6d6c2ed122d7addb58132c2003251b388b525e49
damien911224 <damien911224@gmail.com>
2022-11-18 00:09:10 +0900

    Modified

--- libs/datasets/thumos14.py
+++ libs/datasets/thumos14.py
@@ -155,7 +155,7 @@ class THUMOS14Dataset(Dataset):
         feats = feats[::self.downsample_rate, :]
         feat_stride = self.feat_stride * self.downsample_rate
         # T x C -> C x T
-        # feats = torch.from_numpy(np.ascontiguousarray(feats.transpose()))
+        feats = torch.from_numpy(np.ascontiguousarray(feats.transpose()))
         # len_feats = feats.size(1)
         # if not self.is_training and len_feats > 5000:
         #     resize_feats = F.interpolate(
