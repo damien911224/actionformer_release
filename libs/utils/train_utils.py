c2fbafd69987b0233d4ff3201a46a2b736f9b117
damien911224 <damien911224@gmail.com>
2022-11-18 17:56:37 +0900

    Modified

--- libs/utils/train_utils.py
+++ libs/utils/train_utils.py
@@ -456,8 +456,6 @@ def train_one_epoch_phase_2(
             this_proposals = proposals[:, start_index:start_index + this_len]
             pyramidal_proposals.append(this_proposals)
             start_index += this_len
-            print(this_proposals.shape)
-        exit()
 
         detr_predictions = detr(features, pyramidal_proposals, detr_target_dict)
         loss_dict = criterion(detr_predictions, detr_target_dict)
