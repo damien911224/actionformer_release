fa1a3dae95fb2f22516884bcf8f23adefda23668
damien911224 <damien911224@gmail.com>
2022-11-24 00:51:55 +0900

    Modified

--- libs/modeling/detr.py
+++ libs/modeling/detr.py
@@ -71,7 +71,7 @@ class DINO(nn.Module):
         self.feat_label_enc = nn.Embedding(200 + 1, hidden_dim)
         self.feat_score_enc = nn.Linear(1, hidden_dim)
         self.feat_box_enc = nn.Linear(2, hidden_dim)
-        self.feat_level_enc = nn.Linear(10, hidden_dim)
+        self.feat_level_enc = nn.Embedding(10, hidden_dim)
         if not use_dab:
             self.query_embed = nn.Embedding(num_queries, hidden_dim * 2)
         else:
