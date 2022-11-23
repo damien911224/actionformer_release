c3ad504f52fe6ca922269023c666a69e4f2534e4
damien911224 <damien911224@gmail.com>
2022-11-23 20:42:19 +0900

    Modified

--- libs/modeling/deformable_transformer.py
+++ libs/modeling/deformable_transformer.py
@@ -643,7 +643,7 @@ class DeformableTransformerDecoder(nn.Module):
             # hack implementation for iterative bounding box refinement
             if self.bbox_embed is not None:
                 tmp = self.bbox_embed[lid](output)
-                tmp[100:] = torch.zeros_like(tmp[100:])
+                tmp[100:] = tmp[100:] * torch.zeros_like(tmp[100:])
                 if reference_points.shape[-1] == 4:
                     new_reference_points = tmp + inverse_sigmoid(reference_points)
                     new_reference_points = new_reference_points.sigmoid()
