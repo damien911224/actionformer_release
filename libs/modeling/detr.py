f802e6d7885ab8dfa498b8131ea0b243e144abff
damien911224 <damien911224@gmail.com>
2022-11-24 01:37:40 +0900

    Modified

--- libs/modeling/detr.py
+++ libs/modeling/detr.py
@@ -194,7 +194,7 @@ class DINO(nn.Module):
             prop_label_embeds = self.feat_label_enc(prop_labels.long())
             prop_score_embeds = self.feat_score_enc(prop_scores)
             # prop_level_embeds = self.feat_level_enc(torch.ones_like(prop_labels.long()) * l)
-            box_src = (prop_box_embeds + prop_label_embeds + prop_score_embeds + prop_level_embeds).permute(0, 2, 1)
+            box_src = (prop_box_embeds + prop_label_embeds + prop_score_embeds).permute(0, 2, 1)
             # box_src = (prop_box_embeds + prop_score_embeds).permute(0, 2, 1)
             # src = feat
             n, c, t = box_src.shape
