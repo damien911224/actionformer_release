57b6afdc59799868449789193401f097f1d41f91
damien911224 <damien911224@gmail.com>
2022-11-24 17:58:48 +0900

    Modified

--- libs/modeling/detr.py
+++ libs/modeling/detr.py
@@ -63,10 +63,10 @@ class DINO(nn.Module):
         self.use_dab = use_dab
         self.num_patterns = num_patterns
         self.random_refpoints_xy = random_refpoints_xy
-        # self.label_enc = nn.Embedding(dn_labelbook_size + 1, hidden_dim)
+        self.dn_label_enc = nn.Embedding(dn_labelbook_size + 1, hidden_dim)
         self.query_label_enc = nn.Embedding(200 + 1, hidden_dim)
         self.query_score_enc = nn.Linear(1, hidden_dim)
-        self.query_box_enc = nn.Linear(2, hidden_dim)
+        self.query_type_enc = nn.Linear(2, hidden_dim)
         self.feat_label_enc = nn.Embedding(200 + 1, hidden_dim)
         self.feat_score_enc = nn.Linear(1, hidden_dim)
         self.feat_box_enc = nn.Linear(2, hidden_dim)
@@ -224,7 +224,8 @@ class DINO(nn.Module):
         prop_scores = proposals[..., -1].unsqueeze(-1)
         prop_label_embeds = self.query_label_enc(prop_labels.long())
         prop_score_embeds = self.query_score_enc(prop_scores)
-        prop_query_label = prop_label_embeds + prop_score_embeds
+        prop_type_embeds = self.query_type_enc.weight.unsqueeze(0)
+        prop_query_label = prop_label_embeds + prop_score_embeds + prop_type_embeds
         prop_query_bbox = torch.cat([proposals[..., 1:-1],
                                       ((proposals[..., 1] + proposals[..., 2]) / 2.0).unsqueeze(-1),
                                       (proposals[..., 2] - proposals[..., 1]).unsqueeze(-1)], dim=-1)
@@ -238,7 +239,7 @@ class DINO(nn.Module):
             dino_query_label, dino_query_bbox, attn_mask, dn_meta = \
                 prepare_for_cdn(dn_args=(targets, self.dn_number, self.dn_label_noise_ratio, self.dn_box_noise_scale),
                                 training=self.training, num_queries=self.num_queries, num_classes=self.num_classes,
-                                hidden_dim=self.hidden_dim, label_enc=self.feat_label_enc)
+                                hidden_dim=self.hidden_dim, label_enc=self.dn_label_enc)
             input_query_label = torch.cat((dino_query_label, input_query_label), dim=1)
             input_query_bbox = torch.cat((dino_query_bbox, input_query_bbox), dim=1)
         else:
@@ -260,10 +261,10 @@ class DINO(nn.Module):
 
         hs, init_reference, inter_references, _, _ = \
             self.transformer(srcs, pos_1d, pos_2d, box_srcs, box_pos_1d, box_pos_2d,
-                             query_embeds, attn_mask, self.feat_label_enc)
+                             query_embeds, attn_mask, self.dn_label_enc)
 
         # In case num object=0
-        hs[0] += self.feat_label_enc.weight[0, 0] * 0.0
+        hs[0] += self.dn_label_enc.weight[0, 0] * 0.0
 
         outputs_classes = []
         outputs_coords = []
