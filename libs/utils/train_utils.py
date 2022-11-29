be6e72392cd51d1d72dfb4cb2bb76b2a161a3db9
damien911224 <damien911224@gmail.com>
2022-11-29 16:58:41 +0900

    Modified

--- libs/utils/train_utils.py
+++ libs/utils/train_utils.py
@@ -788,9 +788,9 @@ def valid_one_epoch_phase_2(
             detr_scores, labels = torch.max(logits, dim=-1)
             scores = detr_scores
 
-            # boxes = boxes[:, :100]
-            # labels = labels[:, :100]
-            # scores = scores[:, :100]
+            boxes = boxes[:, :100]
+            labels = labels[:, :100]
+            scores = scores[:, :100]
 
             # mean_proposals = torch.mean(torch.stack(proposals, dim=0), dim=0)
             # dense_boxes = mean_proposals[..., 1:3]
