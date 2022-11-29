fc124d49bd4fa14fb17254a1af84131f758c657e
damien911224 <damien911224@gmail.com>
2022-11-29 18:18:29 +0900

    Modified

--- libs/modeling/detr.py
+++ libs/modeling/detr.py
@@ -492,7 +492,7 @@ class SetCriterion_DINO(nn.Module):
         device = next(iter(outputs.values())).device
         outputs_without_aux_and_props = {k: v[:, :100] for k, v in outputs_without_aux.items()
                                          if k in ["pred_logits", "pred_boxes"]}
-        indices = self.matcher(outputs_without_aux_and_props, targets)
+        indices = self.matcher(outputs_without_aux_and_props, targets, layer=4)
         entire_indices = self.matcher(outputs_without_aux, targets, layer=4)
 
         if return_indices:
@@ -553,7 +553,7 @@ class SetCriterion_DINO(nn.Module):
 
         for loss in self.losses:
             # losses.update(self.get_loss(loss, outputs, targets, indices, num_boxes))
-            losses.update(self.get_loss(loss, outputs_without_aux_and_props, targets, indices, num_boxes))
+            losses.update(self.get_loss(loss, outputs_without_aux_and_props, targets, indices, num_boxes, layer=4))
         losses.update(self.get_loss("labels", outputs_without_aux, targets, entire_indices, num_boxes,
                                     name="loss_entire_ce", layer=4))
 
@@ -564,7 +564,7 @@ class SetCriterion_DINO(nn.Module):
                 outputs_without_props = {k: v[:, :100] for k, v in aux_outputs.items()
                                          if k in ["pred_logits", "pred_boxes"]}
                 # indices = self.matcher(outputs_without_props, targets, layer=idx)
-                indices = self.matcher(outputs_without_props, targets)
+                indices = self.matcher(outputs_without_props, targets, layer=4)
                 # entire_indices = self.matcher(aux_outputs, targets, layer=idx)
                 entire_indices = self.matcher(aux_outputs, targets, layer=4)
                 if return_indices:
@@ -574,6 +574,7 @@ class SetCriterion_DINO(nn.Module):
                     if loss == "labels":
                         kwargs['log'] = False
                     # kwargs['layer'] = idx
+                    kwargs['layer'] = 4
                     l_dict = self.get_loss(loss, outputs_without_props, targets, indices, num_boxes, **kwargs)
                     l_dict = {k + f'_{idx}': v for k, v in l_dict.items()}
                     losses.update(l_dict)
