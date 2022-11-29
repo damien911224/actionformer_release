0f7eb505875488dc8eb07dee472b26719f8f4f9c
damien911224 <damien911224@gmail.com>
2022-11-29 20:03:22 +0900

    Modified

--- libs/modeling/deformable_transformer.py
+++ libs/modeling/deformable_transformer.py
@@ -561,13 +561,13 @@ class DeformableTransformerDecoderLayer(nn.Module):
         tgt = tgt + self.dropout2(tgt2)
         tgt = self.norm2(tgt)
 
-        # # cross attention
-        # tgt2 = self.cross_attn(self.with_pos_embed(tgt, query_pos),
-        #                          reference_points,
-        #                          self.with_pos_embed(src, box_features + src_pos),
-        #                          src_spatial_shapes, level_start_index, src_padding_mask)
-        # tgt = tgt + self.dropout1(tgt2)
-        # tgt = self.norm1(tgt)
+        # cross attention
+        tgt2 = self.cross_attn(self.with_pos_embed(tgt, query_pos),
+                                 reference_points,
+                                 self.with_pos_embed(src, src_pos),
+                                 src_spatial_shapes, level_start_index, src_padding_mask)
+        tgt = tgt + self.dropout1(tgt2)
+        tgt = self.norm1(tgt)
 
         tgt2 = self.cross_attn_2(self.with_pos_embed(tgt, query_pos),
                                  reference_points,
