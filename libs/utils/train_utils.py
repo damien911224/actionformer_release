8134320be7586b7a788d9e3eb4c06247565a8e3d
damien911224 <damien911224@gmail.com>
2022-11-30 17:57:30 +0900

    Modified

--- libs/utils/train_utils.py
+++ libs/utils/train_utils.py
@@ -879,7 +879,7 @@ def valid_one_epoch(
 
     # log mAP to tb_writer
     if tb_writer is not None:
-        tb_writer.add_scalar('validation/backbone_mAP', mAP, curr_epoch)
+        tb_writer.add_scalar('validation/mAP', mAP, curr_epoch)
 
     return mAP
 
