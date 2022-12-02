58ee17ec046c1397581ee264d7fbc4fd201eb847
damien911224 <damien911224@gmail.com>
2022-12-02 19:52:56 +0900

    Modified

--- libs/modeling/detr.py
+++ libs/modeling/detr.py
@@ -249,7 +249,7 @@ class DINO(nn.Module):
         # query_embeds = torch.cat((input_query_label, input_query_bbox), dim=2)
 
         proposals = torch.cat(proposals, dim=1)
-        prop_query_label = self.prop_label_enc(proposals[..., -1].unsqueeze(-1).long())
+        prop_query_label = self.prop_label_enc(proposals[..., 0].long())
         prop_query_label = prop_query_label + self.prop_score_enc(proposals[..., -1].unsqueeze(-1))
         prop_query_bbox = torch.cat([proposals[..., 1:-1],
                                      ((proposals[..., 1] + proposals[..., 2]) / 2.0).unsqueeze(-1),
