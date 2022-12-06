4971362b30d82d1042bccd190710ffe704b61ae7
damien911224 <damien911224@gmail.com>
2022-12-06 20:22:15 +0900

    Modified

--- libs/utils/train_utils.py
+++ libs/utils/train_utils.py
@@ -902,7 +902,8 @@ def valid_one_epoch_zoom_in(
             boxes = list()
             labels = list()
             scores = list()
-            for l_i in range(4):
+            num_levels = np.minimum(np.log2(max(feat_len // 192, 1)).astype(np.int32), 4)
+            for l_i in range(num_levels):
                 start_indices = np.arange(0, feat_len, feat_len // (2 ** l_i))
                 for s_i, start_index in enumerate(start_indices):
                     if s_i >= len(start_indices) - 1:
