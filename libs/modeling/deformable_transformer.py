b073516060588dd689917114f32ab080627a48af
damien911224 <damien911224@gmail.com>
2022-12-14 15:36:43 +0900

    Modified

--- libs/modeling/deformable_transformer.py
+++ libs/modeling/deformable_transformer.py
@@ -520,8 +520,8 @@ class DeformableTransformerDecoderLayer(nn.Module):
         self.norm1 = nn.LayerNorm(d_model)
 
         # self attention
-        self.self_attn = nn.MultiheadAttention(d_model, n_heads, dropout=dropout)
-        # self.self_attn = MSDeformAttn(d_model, 6, n_heads, n_points)
+        # self.self_attn = nn.MultiheadAttention(d_model, n_heads, dropout=dropout)
+        self.self_attn = MSDeformAttn(d_model, 6, n_heads, n_points)
         self.dropout2 = nn.Dropout(dropout)
         self.norm2 = nn.LayerNorm(d_model)
 
