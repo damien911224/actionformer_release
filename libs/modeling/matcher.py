0b0c7d772477f1364046f3a929d35113a4ba54b1
damien911224 <damien911224@gmail.com>
2022-12-15 20:29:38 +0900

    Modified

--- libs/modeling/matcher.py
+++ libs/modeling/matcher.py
@@ -51,7 +51,8 @@ class HungarianMatcher(nn.Module):
             bs, num_queries = outputs["pred_logits"].shape[:2]
 
             # We flatten to compute the cost matrices in a batch
-            out_prob = outputs["pred_logits"].flatten(0, 1).softmax(-1)  # [batch_size * num_queries, num_classes]
+            # out_prob = outputs["pred_logits"].flatten(0, 1).softmax(-1)  # [batch_size * num_queries, num_classes]
+            out_prob = outputs["pred_logits"].flatten(0, 1).sigmoid()  # [batch_size * num_queries, num_classes]
             out_prob = out_prob[..., 0] * 1.0 + out_prob[..., 1] * 0.5
             # out_embeds = outputs["pred_embeds"].flatten(0, 1)
             # out_sims = outputs["pred_sims"].flatten(0, 1).softmax(-1)
