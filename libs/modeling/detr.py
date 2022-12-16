21ef4887cc8a6508a0cf6e0d6b4e6b6c276e2f96
damien911224 <damien911224@gmail.com>
2022-12-16 16:47:51 +0900

    Modified

--- libs/modeling/detr.py
+++ libs/modeling/detr.py
@@ -357,7 +357,7 @@ class SetCriterion_DINO(nn.Module):
             target_classes_onehot.scatter_(2, target_classes.unsqueeze(-1) + i * self.num_classes, 1.0)
 
         print(target_classes_onehot.shape)
-        exit()
+        exit(src_logits.shape)
 
         target_classes_onehot = target_classes_onehot[:, :, :-1]
         if layer is not None:
