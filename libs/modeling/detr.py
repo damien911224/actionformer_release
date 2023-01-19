2a8e8621421b62ccf9025b43b937d22450e61e09
damien911224 <damien911224@gmail.com>
2023-01-19 23:23:21 +0900

    Modified

--- libs/modeling/detr.py
+++ libs/modeling/detr.py
@@ -484,10 +484,6 @@ class SetCriterion_DINO(nn.Module):
         # N, Q, 1
         valid_masks = torch.stack(valid_masks, dim=0).cuda()
 
-        idx = self._get_src_permutation_idx(indices[0])
-        print(valid_masks[idx])
-        exit()
-
         target_classes = torch.full(src_logits.shape[:2], self.num_classes * 1, dtype=torch.int64, device=src_logits.device)
         target_classes_onehot = torch.zeros([src_logits.shape[0], src_logits.shape[1], src_logits.shape[2] + 1],
                                             dtype=src_logits.dtype, layout=src_logits.layout, device=src_logits.device)
