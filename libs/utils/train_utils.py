37efd1a35a3d4d96d5e16e7299b5bdf88f89d340
damien911224 <damien911224@gmail.com>
2023-01-26 21:18:45 +0900

    Modified

--- libs/utils/train_utils.py
+++ libs/utils/train_utils.py
@@ -438,8 +438,8 @@ def train_one_epoch(
                                              (boxes[..., 1] - boxes[..., 0]).unsqueeze(-1)), dim=-1).cuda()
             detr_target_dict.append(batch_dict)
 
-        features = [feat for feat in backbone_features]
-        # features = [torch.stack([x["feats"] for x in video_list], dim=0).cuda()]
+        # features = [feat for feat in backbone_features]
+        features = [torch.stack([x["feats"] for x in video_list], dim=0).cuda()]
 
         labels = list()
         scores = list()
@@ -1098,8 +1098,8 @@ def valid_one_epoch(
             segments = torch.stack(segments, dim=0)
             proposals = torch.cat((labels.unsqueeze(-1), segments, scores.unsqueeze(-1)), dim=-1).cuda()
 
-            features = [feat for feat in backbone_features]
-            # features = [torch.stack([x["feats"] for x in video_list], dim=0).cuda()]
+            # features = [feat for feat in backbone_features]
+            features = [torch.stack([x["feats"] for x in video_list], dim=0).cuda()]
 
             start_index = 0
             pyramidal_proposals = list()
