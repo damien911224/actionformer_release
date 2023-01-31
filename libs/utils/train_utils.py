6c63c5dc67290e12aaf94e156ddb106f0820b892
damien911224 <damien911224@gmail.com>
2023-01-31 20:08:24 +0900

    Modified

--- libs/utils/train_utils.py
+++ libs/utils/train_utils.py
@@ -479,9 +479,9 @@ def train_one_epoch(
         N, P, _ = segments.shape
         segments_input = segments.view((N * P, 2))
         IoU_mat = segment_ops.segment_iou(segments_input, segments_input)
-        IoUs = IoU_mat.max(dim=1)[0]
+        IoUs = IoU_mat.max(dim=1)[0].view((N, P))
         high_IoU_flags = IoUs >= 0.60
-        high_IoU_proposals = torch.where(high_IoU_flags[..., None], proposals, torch.zeros_like(proposals)).cuda()
+        high_IoU_proposals = torch.where(high_IoU_flags, proposals, torch.zeros_like(proposals)).cuda()
 
         # start_index = 0
         # pyramidal_proposals = list()
@@ -1118,9 +1118,9 @@ def valid_one_epoch(
             N, P, _ = segments.shape
             segments_input = segments.view((N * P, 2))
             IoU_mat = segment_ops.segment_iou(segments_input, segments_input)
-            IoUs = IoU_mat.max(dim=1)[0]
+            IoUs = IoU_mat.max(dim=1)[0].view((N, P))
             high_IoU_flags = IoUs >= 0.60
-            high_IoU_proposals = torch.where(high_IoU_flags[..., None], proposals, torch.zeros_like(proposals)).cuda()
+            high_IoU_proposals = torch.where(high_IoU_flags, proposals, torch.zeros_like(proposals)).cuda()
 
             features = [feat for feat in backbone_features]
             # features = [torch.stack([x["feats"] for x in video_list], dim=0).cuda()]
