7d4f757ff249119de552bdcd82145af38f841916
damien911224 <damien911224@gmail.com>
2023-02-16 22:28:18 +0900

    Modified

--- libs/utils/misc.py
+++ libs/utils/misc.py
@@ -381,17 +381,17 @@ def nested_tensor_from_tensor_list(tensor_list: List[Tensor]):
         #     return _onnx_nested_tensor_from_tensor_list(tensor_list)
 
         # TODO make it support different-sized images
-        max_size = _max_by_axis([list(img.shape) for img in tensor_list])
+        # max_size = _max_by_axis([list(img.shape) for img in tensor_list])
         # min_size = tuple(min(s) for s in zip(*[img.shape for img in tensor_list]))
-        batch_shape = [len(tensor_list)] + max_size
-        b, c, w = batch_shape
+        # batch_shape = [len(tensor_list)] + max_size
+        b, c, w = tensor_list[0]
         dtype = tensor_list[0].dtype
         device = tensor_list[0].device
-        tensor = torch.zeros(batch_shape, dtype=dtype, device=device)
+        tensor = torch.zeros(b, dtype=dtype, device=device)
         mask = torch.ones((b, w), dtype=torch.bool, device=device)
-        for img, pad_img, m in zip(tensor_list, tensor, mask):
-            pad_img[: img.shape[0], : img.shape[1]].copy_(img)
-            m[: img.shape[1]] = False
+        # for img, pad_img, m in zip(tensor_list, tensor, mask):
+        #     pad_img[: img.shape[0], : img.shape[1]].copy_(img)
+        #     m[: img.shape[1]] = False
     else:
         raise ValueError('not supported')
     return NestedTensor(tensor, mask)
