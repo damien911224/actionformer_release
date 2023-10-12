1d220aed02a5ee5a604f6f0fdb4e0ce155b61df8
damien <damien911224@gmail.com>
2023-10-12 17:07:45 +0900

    Modified

--- train_origin.py
+++ train_origin.py
@@ -10,7 +10,7 @@ import torch
 import torch.nn as nn
 import torch.utils.data
 # for visualization
-from torch.utils.tensorboard import SummaryWriter
+from tensorboardX import SummaryWriter
 
 # our code
 from libs.core import load_config
