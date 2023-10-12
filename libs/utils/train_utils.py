ce659f16bc66901bae7a3dd1ab9b711689fdbc30
damien <damien911224@gmail.com>
2023-10-12 17:06:38 +0900

    Modified

--- libs/utils/train_utils.py
+++ libs/utils/train_utils.py
@@ -388,7 +388,7 @@ def train_one_epoch_zoom_in(
     print("[Train]: Epoch {:d} finished with lr={:.8f}\n".format(curr_epoch, lr))
     return
 
-def train_one_epoch(
+def train_one_epoch_kept(
         train_loader,
         backbone,
         backbone_optimizer,
@@ -601,6 +601,115 @@ def train_one_epoch(
 
     # finish up and print
     lr = backbone_scheduler.get_last_lr()[0]
+    print("[Train]: Epoch {:d} finished with lr={:.8f}\n".format(curr_epoch, lr))
+    return
+
+def train_one_epoch(
+    train_loader,
+    model,
+    optimizer,
+    scheduler,
+    curr_epoch,
+    model_ema = None,
+    clip_grad_l2norm = -1,
+    tb_writer = None,
+    print_freq = 20
+):
+    """Training the model for one epoch"""
+    # set up meters
+    batch_time = AverageMeter()
+    losses_tracker = {}
+    # number of iterations per epoch
+    num_iters = len(train_loader)
+    # switch to train mode
+    model.train()
+
+    # main training loop
+    print("\n[Train]: Epoch {:d} started".format(curr_epoch))
+    start = time.time()
+    for iter_idx, video_list in enumerate(train_loader, 0):
+        # zero out optim
+        optimizer.zero_grad(set_to_none=True)
+        # forward / backward the model
+        losses = model(video_list)
+        losses['final_loss'].backward()
+        # gradient cliping (to stabilize training if necessary)
+        if clip_grad_l2norm > 0.0:
+            torch.nn.utils.clip_grad_norm_(
+                model.parameters(),
+                clip_grad_l2norm
+            )
+        # step optimizer / scheduler
+        optimizer.step()
+        scheduler.step()
+
+        if model_ema is not None:
+            model_ema.update(model)
+
+        # printing (only check the stats when necessary to avoid extra cost)
+        if (iter_idx != 0) and (iter_idx % print_freq) == 0:
+            # measure elapsed time (sync all kernels)
+            torch.cuda.synchronize()
+            batch_time.update((time.time() - start) / print_freq)
+            start = time.time()
+
+            # track all losses
+            for key, value in losses.items():
+                # init meter if necessary
+                if key not in losses_tracker:
+                    losses_tracker[key] = AverageMeter()
+                # update
+                losses_tracker[key].update(value.item())
+
+            # log to tensor board
+            lr = scheduler.get_last_lr()[0]
+            global_step = curr_epoch * num_iters + iter_idx
+            if tb_writer is not None:
+                # learning rate (after stepping)
+                tb_writer.add_scalar(
+                    'train/learning_rate',
+                    lr,
+                    global_step
+                )
+                # all losses
+                tag_dict = {}
+                for key, value in losses_tracker.items():
+                    if key != "final_loss":
+                        tag_dict[key] = value.val
+                tb_writer.add_scalars(
+                    'train/all_losses',
+                    tag_dict,
+                    global_step
+                )
+                # final loss
+                tb_writer.add_scalar(
+                    'train/final_loss',
+                    losses_tracker['final_loss'].val,
+                    global_step
+                )
+
+            # print to terminal
+            block1 = 'Epoch: [{:03d}][{:05d}/{:05d}]'.format(
+                curr_epoch, iter_idx, num_iters
+            )
+            block2 = 'Time {:.2f} ({:.2f})'.format(
+                batch_time.val, batch_time.avg
+            )
+            block3 = 'Loss {:.2f} ({:.2f})\n'.format(
+                losses_tracker['final_loss'].val,
+                losses_tracker['final_loss'].avg
+            )
+            block4 = ''
+            for key, value in losses_tracker.items():
+                if key != "final_loss":
+                    block4  += '\t{:s} {:.2f} ({:.2f})'.format(
+                        key, value.val, value.avg
+                    )
+
+            print('\t'.join([block1, block2, block3, block4]))
+
+    # finish up and print
+    lr = scheduler.get_last_lr()[0]
     print("[Train]: Epoch {:d} finished with lr={:.8f}\n".format(curr_epoch, lr))
     return
 
